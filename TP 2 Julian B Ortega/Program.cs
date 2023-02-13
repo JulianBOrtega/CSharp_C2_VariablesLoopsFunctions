190570f3f0f0cac154a8b3bfcf6de364f64eb89c
Julián <j.gakusei11@gmail.com>
2023-02-13 19:55:11 -0300

    Add project files.

---
+++ TP 2 Julian B Ortega/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_2_Julian_B_Ortega
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
